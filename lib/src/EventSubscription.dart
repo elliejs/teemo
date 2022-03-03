4a7f0ddbe4aa6214bdbd4acd59ce03d57d3d7df0
Eleanor J S <ejsilver@wpi.edu>
2022-03-03 16:26:40 -0500

    flutter format .

--- lib/src/EventSubscription.dart
+++ lib/src/EventSubscription.dart
@@ -2,32 +2,39 @@ typedef JsonData = Map<String, dynamic>;
 typedef Handler = Future<void> Function(JsonData data);
 
 class EventCode {
-  static const int SUBSCRIBE   = 5;
+  static const int SUBSCRIBE = 5;
   static const int UNSUBSCRIBE = 6;
-  static const int RESPONSE    = 8;
+  static const int RESPONSE = 8;
 }
 
 class EventSubscription {
   Map<String, List<Handler>> _registeredUris = {};
   Map<String, List<Handler>> _registeredPaths = {};
-  Handler defaultBehavior;
+  Handler _defaultBehavior;
 
   static Future<void> _defaultBehaviorImpl(JsonData data) async {
     print(data);
   }
 
-  EventSubscription([this.defaultBehavior=EventSubscription._defaultBehaviorImpl]);
+  EventSubscription(
+      [this._defaultBehavior = EventSubscription._defaultBehaviorImpl]);
 
-  void filterEndpoint(String endpoint, {Handler behavior=EventSubscription._defaultBehaviorImpl}) {
-    if (endpoint.endsWith('*')) { //then this is a path and we want to glob match
-      this._registeredPaths.update(endpoint.substring(0, endpoint.length - 1), (list) => list + [behavior], ifAbsent: () => [behavior]);
+  void filterEndpoint(String endpoint,
+      {Handler behavior = EventSubscription._defaultBehaviorImpl}) {
+    if (endpoint.endsWith('*')) {
+      //then this is a path and we want to glob match
+      this._registeredPaths.update(endpoint.substring(0, endpoint.length - 1),
+          (list) => list + [behavior],
+          ifAbsent: () => [behavior]);
     } else {
-      this._registeredUris.update(endpoint, (list) => list + [behavior], ifAbsent: () => [behavior]);
+      this._registeredUris.update(endpoint, (list) => list + [behavior],
+          ifAbsent: () => [behavior]);
     }
   }
 
   void unfilterEndpoint(String endpoint) {
-    if (endpoint.endsWith('*')) { //then this is a path and we want to glob match
+    if (endpoint.endsWith('*')) {
+      //then this is a path and we want to glob match
       this._registeredPaths.remove(endpoint.substring(0, endpoint.length - 1));
     } else {
       this._registeredUris.remove(endpoint);
@@ -37,15 +44,12 @@ class EventSubscription {
   List<Future<void>> tasks(JsonData data) {
     List<Handler> tasks = [];
 
-    this._registeredPaths.forEach((key, value) => {
-      data['uri'].startsWith(key)
-      ? tasks.addAll(value)
-      : {}
-    });
+    this._registeredPaths.forEach((key, value) =>
+        {data['uri'].startsWith(key) ? tasks.addAll(value) : {}});
 
     tasks.addAll(this._registeredUris[data['uri']] ?? []);
 
-    if (tasks.isEmpty) tasks.add(this.defaultBehavior);
+    if (tasks.isEmpty) tasks.add(this._defaultBehavior);
     return tasks.map((e) => e(data)).toList();
   }
 }
--- lib/src/teemo_impl.dart
+++ lib/src/teemo_impl.dart
@@ -14,22 +14,23 @@ class Teemo {
 
   Map<String, List<EventSubscription>> _subscriptions;
 
-  Teemo._create(this._authKey, this._port, this._restClient, this._websocket): _subscriptions={};
+  Teemo._create(this._authKey, this._port, this._restClient, this._websocket)
+      : _subscriptions = {};
 
   static Future<Teemo> create() async {
     String authKey = '';
     int port = -1;
 
-    if (Platform.isMacOS || Platform.isLinux) { //we can run ps aux
+    if (Platform.isMacOS || Platform.isLinux) {
+      //we can run ps aux
       ProcessResult psRes = await Process.run('ps', ['aux']);
       List<String> args = psRes.stdout.split(' ');
 
-      for(int i = 0; i < args.length; i++) {
+      for (int i = 0; i < args.length; i++) {
         List<String> keyVal = args[i].split('=');
         if (keyVal[0] == '--remoting-auth-token') {
           authKey = keyVal[1];
-        }
-        else if (keyVal[0] == '--app-port') {
+        } else if (keyVal[0] == '--app-port') {
           try {
             port = int.parse(keyVal[1]);
           } catch (error) {
@@ -42,44 +43,54 @@ class Teemo {
       }
     }
 
-    if(authKey == '' || port == -1) print('something\'s gone wrong');
+    if (authKey == '' || port == -1) print('something\'s gone wrong');
 
     print(authKey + ':' + port.toString());
 
-    String cert = await rootBundle.loadString('packages/teemo/assets/riotgames.pem');
+    String cert =
+        await rootBundle.loadString('packages/teemo/assets/riotgames.pem');
     print(cert);
     SecurityContext secCtx = SecurityContext();
     secCtx.setTrustedCertificatesBytes(utf8.encode(cert));
 
-    Teemo teemo = Teemo._create(authKey, port, HttpClient(context: secCtx), await WebSocket.connect(
-      'wss://127.0.0.1:$port',
-      headers: {
-      'Authorization': 'Basic ' + utf8.fuse(base64).encode('riot:$authKey')
-      },
-      customClient: HttpClient(context: secCtx) //TODO: try teemo.rest_client
-    ));
+    Teemo teemo = Teemo._create(
+        authKey,
+        port,
+        HttpClient(context: secCtx),
+        await WebSocket.connect('wss://127.0.0.1:$port',
+            headers: {
+              'Authorization':
+                  'Basic ' + utf8.fuse(base64).encode('riot:$authKey')
+            },
+            customClient:
+                HttpClient(context: secCtx) //TODO: try teemo.rest_client
+            ));
     teemo._startWebsocketListener();
     return teemo;
   }
 
   void _startWebsocketListener() {
     this._websocket.listen((data) {
-      if(data.isEmpty) return;
+      if (data.isEmpty) return;
       List<dynamic> jsonData = json.decode(data);
-      List<EventSubscription> subscriptions = this._subscriptions[jsonData[1]] ?? [];
+      List<EventSubscription> subscriptions =
+          this._subscriptions[jsonData[1]] ?? [];
       subscriptions.forEach((sub) => sub.tasks(jsonData[2]));
     });
   }
 
-  EventSubscription subscribe(String event, {EventSubscription? subscription, Handler? behavior}) {
-    EventSubscription _subscription = subscription ?? (behavior != null ? EventSubscription(behavior) : EventSubscription());
-    this._subscriptions.update(event, (list) => list + [_subscription], ifAbsent: () => [_subscription]);
+  EventSubscription subscribe(String event,
+      {EventSubscription? subscription, Handler? behavior}) {
+    EventSubscription _subscription = subscription ??
+        (behavior != null ? EventSubscription(behavior) : EventSubscription());
+    this._subscriptions.update(event, (list) => list + [_subscription],
+        ifAbsent: () => [_subscription]);
     this._websocket.add('[${EventCode.SUBSCRIBE}, "$event"]');
     return _subscription;
   }
 
   void unsubscribe(String event, {EventSubscription? subscription}) {
-    if(subscription != null) {
+    if (subscription != null) {
       this._subscriptions[event]?.remove(subscription);
       if (this._subscriptions[event]?.isEmpty ?? true) {
         this._subscriptions.remove(event);
@@ -96,32 +107,37 @@ class Teemo {
     return this._subscriptions[event] ?? [];
   }
 
-  void subscriptionFilterEndpoint(EventSubscription subscription, String endpoint, {Handler? behavior}) {
-    behavior != null ? subscription.filterEndpoint(endpoint, behavior: behavior) : subscription.filterEndpoint(endpoint);
+  void subscriptionFilterEndpoint(
+      EventSubscription subscription, String endpoint,
+      {Handler? behavior}) {
+    behavior != null
+        ? subscription.filterEndpoint(endpoint, behavior: behavior)
+        : subscription.filterEndpoint(endpoint);
   }
 
-  void subscriptionUnfilterEndpoint(EventSubscription subscription, String endpoint) {
+  void subscriptionUnfilterEndpoint(
+      EventSubscription subscription, String endpoint) {
     subscription.unfilterEndpoint(endpoint);
   }
 
-  Future<JsonData> request(String method, String endpoint) async {
-    print('getting');
-    late HttpClientRequest req;
-    switch (method) {
-      case 'get':
-      case 'GET':
-        req = await this._restClient.getUrl(Uri.parse('https://127.0.0.1:${this._port}$endpoint'));
-        break;
-      default:
-        return Future<JsonData>.value({'error': 'no method: $method'});
-    }
+  Future<JsonData> request(String method, String endpoint,
+      {JsonData? body}) async {
+    HttpClientRequest req = await this
+        ._restClient
+        .openUrl(method, Uri.parse('https://127.0.0.1:${this._port}$endpoint'));
 
     req.headers.set(HttpHeaders.acceptHeader, 'application/json');
     req.headers.set(HttpHeaders.contentTypeHeader, 'application/json');
-    req.headers.set(HttpHeaders.authorizationHeader, 'Basic ' + utf8.fuse(base64).encode('riot:${this._authKey}'));
+    req.headers.set(HttpHeaders.authorizationHeader,
+        'Basic ' + utf8.fuse(base64).encode('riot:${this._authKey}'));
+
+    if (body != null) req.add(json.fuse(utf8).encode(body));
 
     HttpClientResponse resp = await req.close();
-    return resp.transform(utf8.decoder).join().then((data) => json.decode(data));
+    return resp
+        .transform(utf8.decoder)
+        .join()
+        .then((data) => json.decode(data));
   }
 
   void close() {
